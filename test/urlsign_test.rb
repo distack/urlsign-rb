4b2fe0c55b526aa090ab3afb7f1d6ff9d342590e
Ben Fischer <ben.fischer.810@gmail.com>
2017-01-12 09:19:40 -0700

    test that the signed URL is valid without a query string

--- test/urlsign_test.rb
+++ test/urlsign_test.rb
@@ -5,13 +5,22 @@ class Distack::URLSignTest < Minitest::Test
     refute_nil ::Distack::URLSign::VERSION
   end
 
-  def test_sign_verify
+  def test_sign_verify_with_query_string
     url = URI.parse("http://foo.test/path?search=john&fields[]=name&fields[]=age")
     key = "a1b2c3d4f5"
 
     signer = Distack::URLSign::Signer.new(key)
     signed_url = signer.sign(url)
     assert_equal signer.verify(signed_url), url
+  end
+
+  def test_sign_valid_without_query_string
+    url = URI.parse("http://foo.test:3000/search")
+    key = "a1b2c3d4f5"
+
+    signer = Distack::URLSign::Signer.new(key)
+    signed_url = signer.sign(url).to_s
+    assert_equal true, signer.valid?(URI.parse(signed_url))
   end
 
   def test_valid
